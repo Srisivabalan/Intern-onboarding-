3864a0ec35cb83060fe1fb69eea96cfce915f361
Sri Sivabalan B <sivajuley4499@gmail.com>
2026-01-14 12:11:36 +0530

    task 7 upadate

--- frontend/Pages/Index.js
+++ frontend/Pages/Index.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to my Next.js app</p>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
